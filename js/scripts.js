a2e809e6b05860b8b442b9ea46d120b10bac2e26
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-11 12:49:59 -0700

    Add another result for people that might not be suited for programming>

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,14 @@ $(document).ready(function(){
     var answer4 = $("#question4").val();
     var answer5 = $("#question5").val();
 
-    if(answer1 === "You obsese over layouts, navigation, colors, and design" && answer3 === "Websites and webapps" ){
+    if(answer1 === "You obsese over layouts, navigation, colors, and design" && answer3 === "Websites and webapps" && answer4 === "No" && answer5 === "No" ){
       $("#result1").show();
-    }else if(answer1 === "You obsese over layouts, navigation, colors, and design" && answer3 === "Mobile Apps" ){
-        $("#result2").show();
-    }else if(answer1 === "Do you like to create logical computations" && answer3 === "Software programming" ){
-          $("#result3").show();
+    }else if(answer1 === "You obsese over layouts, navigation, colors, and design" && answer3 === "Mobile Apps" && answer4 === "No" && answer5 === "No"){
+      $("#result2").show();
+    }else if(answer1 === "Do you like to create logical computations" && answer3 === "Software programming" && answer4 === "No" && answer5 === "No" ){
+      $("#result3").show();
+    }else if(answer4 === "No" || answer5 === "No" ){
+      $("#result5").show();
     } else{
       $("#result4").show();
       }
