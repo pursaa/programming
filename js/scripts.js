0838c27f1c4f1a556f066348ce3d51585669d595
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-11 11:53:19 -0700

    Add two more line is js for results...else statement does not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
     event.preventDefault();
-    var You obsese over layouts, navigation, colors, and design = "1a"
-    var Websites and webapps = "3a"
     var answer1 = $("#question1").val();
     var answer2 = $("#question2").val();
     var answer3 = $("#question3").val();
@@ -11,23 +9,12 @@ $(document).ready(function(){
 
     if(answer1 === "You obsese over layouts, navigation, colors, and design" && answer3 === "Websites and webapps" ){
       $("#result1").show();
+    }else if(answer1 === "You obsese over layouts, navigation, colors, and design" && answer3 === "Mobile Apps" ){
+        $("#result2").show();
+      }else if(answer1 === "Do you like to create logical computations" && answer3 === "Software programming" ){
+          $("#result3").show();
 
-    // }else if (answer1 !== "Beast" && answer2 === "Under the sea" && answer3 === "No" ){
-    // $("#result2").show();
-    //
-    // } else if(answer1 !== "Fox" && answer2==="In the forest" && answer3==="Yes"){
-    //   $("#result2").show();
-    //
-    // } else if(answer1 === "Fox" && answer2==="In the forest" && answer3 ==="Yes"){
-    //   $("#result3").show();
-    //
-    // } else if(answer2 === "In the suburbs"){
-    //   $("#result3").show();
-    // } else if (answer1 === "Beast"){
-    //   $("#result1").show();
-    //
-    // } else if (answer1 === "Lamp" && answer2 === "In the desert" && answer3 === "Yes"){
-    //   $("#result2").show();
+debugger
 
     } else{
       $("#result4").show();
